e40dde3949e8da3227f3b36aa4ad02eaa14fa73b
Anastasiya Besman <staenrey@gmail.com>
2021-02-19 20:17:35 +1100

    Added .ejs files, sql database initialization file, configured nodemon and morgan

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
+// setting up the app and the dependencies
+
 const express = require("express")
 const morgan = require("morgan")
-const path = require("path")
 const expressLayouts = require("express-ejs-layouts")
+const path = require("path")
 
 const app = express()
+const port = 3000
 
+app.set("layout", "pages/layouts/basiclayout")
 app.set("view engine", "ejs")
+app.use(morgan("dev")) // to get morgan working
 
-app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true })) // for parsing app/x-www-form-urlencoded
 
-app.set('views', path.join(__dirname, 'views')) // was in Harry's project (?)
+//app.set('views', path.join(__dirname, 'views')) // was in Harry's project (?)
 app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use(expressLayouts)
+
+
+// files created by us which are used by our app
+
+
+
+// the routes
+
+app.get("/login", (req, res) => {
+    res.render("pages/login")
+})
+
+
+// starting the server
 
 app.listen(3000, () => {
   console.log('listening on 3000')
 })
-
-// this comment will be fetched by Jake
