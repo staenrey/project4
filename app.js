68d8155565984c7b41a2b1cced6762f022f61874
Anastasiya Besman <staenrey@gmail.com>
2021-02-22 11:29:30 +1100

    Basic routes

--- app.js
+++ app.js
@@ -17,17 +17,38 @@ app.use(express.urlencoded({ extended: true })) // for parsing app/x-www-form-ur
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(expressLayouts)
 
-
 // files created by us which are used by our app
 
 app.set("layout", "pages/layouts/basiclayout")
 
 // the routes
 
+// 1) homepage
+app.get("/", (req, res) => {
+  res.render("pages/home")
+})
+
+// 2) login
 app.get("/login", (req, res) => {
     res.render("pages/login")
 })
 
+// 3) logout route (no page)
+
+// 4) employee page
+app.get("/addschedule", (req, res) => {
+  res.render("pages/addschedule")
+})
+
+// 5) schedule management
+app.get("/addschedule", (req, res) => {
+  res.render("pages/addschedule")
+})
+
+// 6) signup
+app.get("/signup", (req, res) => {
+  res.render("pages/signup")
+})
 
 // starting the server
 
