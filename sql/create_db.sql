e610457246948abcb08590ed764a4fff229ee00f
Anastasiya Besman <staenrey@gmail.com>
2021-02-23 18:56:20 +1100

    An ugly navbar for now

--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,9 +1,10 @@
 -- to make database:
 
 CREATE DATABASE project4db;
+
 CREATE USER project4admin WITH ENCRYPTED PASSWORD 'Project4';
 GRANT ALL PRIVILEGES ON DATABASE project4db TO project4admin;
 
 -- then quit
--- run psql -U project4admin project4db
+-- psql -U project4admin project4db
 -- enter password
